16d4378a469118e9903fa04e70bf4e07a0e9a70b
Sylvain Gogel <sylvain.gogel@gmail.com>
2018-04-19 23:51:02 +0200

    enable JWT middleware

--- src/jwt/middleware.js
+++ src/jwt/middleware.js
@@ -1,12 +1,23 @@
-import jwt from 'express-jwt'
+import ejwt from 'express-jwt'
 import getJwtToken from '../http/request'
+import jwt from 'jsonwebtoken'
+
+const audience = 'https://jwt-demo/api'
+const issuer =  'https://jwt-demo'
 
 export const configureJwtMiddleware = () => {
-  return jwt({ 
+  return ejwt({ 
     secret: process.env.JWT_SECRET,
-    audience: 'http://jwt-demo/api',
-    issuer: 'http://jwt-demo',
+    audience: audience,
+    issuer: issuer,
     credentialsRequired: false,
     getToken: getJwtToken
   })
+}
+
+export const createJwtToken = (payload) => {
+  payload.aud = audience
+  payload.iss = issuer
+  console.log(payload)
+  return jwt.sign(payload, process.env.JWT_SECRET)
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import configureJwtMiddleware from './jwt/middleware'
+import {configureJwtMiddleware, createJwtToken} from './jwt/middleware'
+import posts from '../data/posts.json'
 
 dotenv.config()
 
@@ -8,30 +9,48 @@ const app = express()
 const jwtMiddleware = configureJwtMiddleware()
 
 app.get('/', function (req, res) {
-  res.send(JSON.stringify({data: 'Hello World!'}))
+  res.json({data: 'Hello World!'})
 })
 
 // get CSRF
 app.get('/user/login', function(req, res) {
-
+  res.json({csrf_login: 'hdsjhdsquç_è1561564654'})
 })
 
 // get AUTH and deliver JWT Token
 app.post('/user/login', function(req, res) {
+  // @TODO validate credentials
 
+  const token = createJwtToken({ foo: 'bar' })
+  console.log(token)
+  res.setHeader('Authorization', 'Bearer ' + token)  
+  res.json({token: token})
+  
 })
 
 
 // Secured paths
-app.post('/api/v1/posts', jwtMiddleware, function(req, res) {
-
+app.get('/api/v1/posts', jwtMiddleware, function(req, res) {
+  if (!req.user) return res.sendStatus(401)
+  res.json(posts)
 })
 
 // Secured paths
-app.post('/api/v1/post/{id}', jwtMiddleware, function(req, res) {
+app.get('/api/v1/post/:postId', jwtMiddleware, function(req, res) {
+  if (!req.user) return res.sendStatus(401)
 
+  console.log(req.params.postId)
+
+  const post = posts.filter(post => {
+    return post.id == req.params.postId
+  })
+
+  if (post.length == 0) {
+    return res.status(404).send('Not found');
+  }
+  
+  res.json(post[0])
 })
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
